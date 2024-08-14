0c320f3ea5d88c577e86486fd68f02ec0b603c18
HSTvOfficial <jandwaliya223@gmail.com>
2024-08-14 14:56:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7028438277:AAG7DEscZbtAnsAlkHU-xbcY5IsjCVWniSs")
+API_ID = int(os.environ.get("API_ID", "25902118"))
+API_HASH = os.environ.get("API_HASH", "e706f49fa723588bc9629472d0711526")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+OWNER_ID = int(os.environ.get("OWNER_ID", "1326633570"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://rivovok937:k5mdnwoKoPDp1Xu0@cluster0.amln9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001897824286"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "60")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
